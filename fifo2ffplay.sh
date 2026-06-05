cd7029e48dace0113620090cffa0296c0193f0f7
Alex T <lexkie@proton.me>
2026-06-05 09:56:01 -0400

    Add files via upload

--- fifo2ffplay.sh
+++ fifo2ffplay.sh
@@ -4,7 +4,7 @@ ffmpeg -hide_banner -loglevel error -f s16le -ar 44100 -ac 2 -i ~/mpd.fifo \
        -i /storage/emulated/0/dearvr.wav \
        -filter_complex "[0:a][1:a]afir=dry=1:wet=1[out]" \
        -map "[out]" -f wav - | \
-ffplay -nodisp -af "bs2b=fcut=650:feed=9.5,volume=45.0,sofalizer=sofa='/storage/emulated/0/dtf_nh2.sofa'" -fflags nobuffer -flags low_delay -i - >/dev/null 2>&1 &
+ffplay -nodisp -af "aresample,aformat=channel_layouts=stereo,equalizer=f=100:width_type=h:width=50:g=8,bs2b=fcut=650:feed=9.5,volume=25.0,sofalizer=sofa='/storage/emulated/0/ClubFritz1.sofa'" -fflags nobuffer -flags low_delay -i - >/dev/null 2>&1 &
 #!/bin/bash
 # Script to monitor mpd and resume playback if it stops
 
